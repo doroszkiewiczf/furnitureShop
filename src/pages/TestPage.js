a05bedc6f91c4cebed63f0b8c8917ccf546a7c10
fdoroszkiewicz <doroszkiewicz.f@gmail.com>
2019-05-13 02:10:32 +0200

    furniture form updated

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,23 +1,29 @@
 import React, {Component} from 'react';
+import {Modal, Header} from 'semantic-ui-react'
 
 class TestPage extends Component{
     
     render(){
         return(
-            <div>  
-            {
-              this.props.furnitures.map(item =>(
-              this.props.category === item.category&&
-              (
-              <div className="App__ItemInfo" key={item.id}>
-                <span>Numer: {item.id}<br/></span>
-                <span>Kategoria: {item.category}<br/></span>
-                <span>{item.name}<br/></span>
-              </div>)
-              
-            )
-              )}
+          <Modal trigger={
+            <div className="App__ItemInfo">
+              <span>Numer: {"asd"}<br/></span>
+              <span>Kategoria: {"item.category"}<br/></span>
+              <span>{"item.name"}<br/></span>
             </div>
+            } closeIcon>
+              <Modal.Header>Witam</Modal.Header>
+              <Modal.Content image>
+
+                <Modal.Description>
+                  <Header>Witam2</Header>
+                  <p></p>
+                  <p>opis</p>
+                </Modal.Description>
+              </Modal.Content>
+            </Modal>
+                    
+
         )
     }
 }
